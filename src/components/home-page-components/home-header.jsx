d95a06cc477ac1c8f706aacb5b97f49bab6f9b5d
Zeeshan Anwar <zeeshananwar@epochclan.com>
2023-08-07 16:28:24 +0500

    Ticket 1,2,3 done...

--- src/components/home-page-components/home-header.jsx
+++ src/components/home-page-components/home-header.jsx
@@ -3,8 +3,8 @@ import dummy from "../../assets/dummy.png";
 
 export const HomeHeader = ({ firstNews }) => {
   return (
-    <Link to={firstNews.url}>
-      <div className="w-full flex mb-8 max-md:flex-col">
+    <Link to={firstNews.url} target="_blank">
+      <div className="w-full flex mb-8 max-md:flex-col max-md:w-4/5 max-md:m-auto">
         <div className="w-1/2 h-96 max-md:w-full max-md:h-96">
           <img
             className="w-full h-full object-cover rounded-lg"
@@ -13,14 +13,14 @@ export const HomeHeader = ({ firstNews }) => {
           />
         </div>
 
-        <div className="home-header-child-2">
-          <label className="my-2" htmlFor="Date">
+        <div className="home-header-child-2 flex flex-col justify-between">
+          <label className="text-gray-600" htmlFor="Date">
             {firstNews.publishedAt.slice(0, 10)}
           </label>
-          <h1 className="text-6xl font-black max-lg:text-3xl max-md:text-2xl my-4">
-            {firstNews.title.slice(0, 70)}...
+          <h1 className="text-6xl font-black max-lg:text-3xl max-md:text-2xl">
+            {firstNews.title.slice(0, 50)}...
           </h1>
-          <p className="my-2">{firstNews.description}</p>
+          <p>{firstNews.description.slice(0,150)}...</p>
         </div>
       </div>
     </Link>
--- src/components/news-card.jsx
+++ src/components/news-card.jsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 import dummy from "../assets/dummy.png";
 export const NewsCard = ({ cardValue }) => {
   return (
-    <Link to={cardValue.url}>
-      <div className="flex flex-col w-96 max-lg:w-80 max-md:w-full">
+    <Link to={cardValue.url} target="_blank">
+      <div className="flex my-6 flex-col w-96 max-lg:w-80 max-md:w-full">
         <div className=" h-60 max-md:h-96 max-sm:h-60">
           <img
             className="w-full h-full object-cover rounded-lg"
@@ -11,14 +11,14 @@ export const NewsCard = ({ cardValue }) => {
             alt="not found"
           />
         </div>
-        <div className="mt-4 mb-4">
-          <label className="my-4" htmlFor="Date">
+        <div className="mt-4">
+          <label className="text-gray-600" htmlFor="Date">
             {cardValue.publishedAt.slice(0, 10)}
           </label>
-          <h1 className="text-2xl max-lg:text-xl my-4">
+          <h1 className="text-2xl max-lg:text-xl my-2">
             {cardValue.title.slice(0, 20)}...
           </h1>
-          <p className="my-4">
+          <p>
             {cardValue.description && cardValue.description.slice(0, 70)}...
           </p>
         </div>
